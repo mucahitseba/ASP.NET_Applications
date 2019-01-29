7d9b85b06e0e111df5db11f4f1e9729fac3b847e
mucahitseba <mucahitseba@gmail.com>
2019-01-29 11:26:44 +0300

    LayoutMain düzenlendi

---
+++ Admin.Web.UI/Controllers/PartialController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Web.UI.Controllers
+{
+    public class PartialController : Controller
+    {
+        public PartialViewResult DrawerPartial()
+        {
+            return PartialView(viewName: "Partial/_DrawerPartial");
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView(viewName: "Partial/_HeaderPartial");
+        }
+        public PartialViewResult ModalPartial()
+        {
+            return PartialView(viewName: "Partial/_ModalPartial");
+        }
+    }
+}
