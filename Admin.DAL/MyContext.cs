5f3e9db8a7ba3346274ba5d57b1305563f67dc40
mucahitseba <mucahitseba@gmail.com>
2019-01-29 13:41:28 +0300

    Category classı eklendi. Katmanlar eklendi

---
+++ Admin.DAL/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Admin.MODELS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.DAL
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base(nameOrConnectionString:"name=MyCon")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().Property(x => x.TaxRate).HasPrecision(3, scale: 2);
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Admin.MODELS/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using Admin.MODELS.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.MODELS.Entities
+{
+    public class Category:BaseEntity<int>
+    {
+        [StringLength(100,ErrorMessage ="Kategori Adı 3 ile 100 karakter arasında olabilir",MinimumLength =3)]
+        [DisplayName("Kategori Adı")]
+        public string CategoryName { get; set; }
+        [Range(0, 1)]
+        public decimal TaxRate { get; set; } = 0;
+        public int? SupCategoryId { get; set; }
+        [ForeignKey("SupCategoryId")]
+        public virtual Category SupCategory { get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+    }
+}
