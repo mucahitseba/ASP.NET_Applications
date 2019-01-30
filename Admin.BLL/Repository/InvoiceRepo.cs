14493103de7c055c362801f0ce9a3fba490fa11b
mucahitseba <mucahitseba@gmail.com>
2019-01-30 09:34:17 +0300

    Repolar eklendi

---
+++ Admin.BLL/Repository/InvoiceRepo.cs
@@ -0,0 +1,14 @@
+﻿using Admin.MODELS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.BLL.Repository
+{
+    public class InvoiceRepo : RepositoryBase<Invoice, long>
+    {
+
+    }
+}
