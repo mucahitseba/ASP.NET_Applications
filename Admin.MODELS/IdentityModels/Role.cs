20cc0fda6a1ff7cc7b005c1b63ca03182702e505
mucahitseba <mucahitseba@gmail.com>
2019-02-04 09:38:11 +0300

    Identity çalışması

---
+++ Admin.MODELS/IdentityModels/Role.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.MODELS.IdentityModels
+{
+    
+    public class Role:IdentityRole
+    {
+        public Role()
+        {
+
+        }
+        public Role(string description)
+        {
+            Description = description;
+        }
+        [StringLength(50)]
+        public string Description { get; set; }
+    }
+}
